fde4737f847d7920fbb92d9bd8732d24d2168cad
anotheronefrombinary <55737030+anotheronefrombinary@users.noreply.github.com>
2019-10-17 20:57:49 +0400

    Python

---
+++ davaleba3.1.py
@@ -0,0 +1,15 @@
+#შექმენით dictionary სადაც key:value იქნება თქვენი სახელი და ასაკი(KEY იქნება თქვენი სახელი, VALUE იქნება თქვენი ასაკი)
+#დაბეჭდეთ რამდენი key:value წყვილისგან შედგება dictionary.
+#შექმენით tuple, რომელშიც იქნება შემდეგი სიტყვები : guido, tim
+#ამის შემდეგ მომხარებელს მიეცით საშუალება შეიყვანონ სახელი და ასაკი.
+#თუკი სახელი არის tuple-ში, მაშინ მომხარებლის მიერ შეყვანილი სახელი და ასაკი დაამატეთ თავდაპირველად შექმნილ dictionary-ში.
+#ხელახლა დაბეჭდეთ რამდენი key:value წყვილისგან შედგება dictionary.
+#:> Solution:
+bibl = {"levan":18}
+tpl = ("guido", "tim")
+print("dictionaryshi aris: ", len(bibl), "wyvili")
+name = input("Sheiyvanet tqveni saxeli: ")
+age = input("Sheiyvanet tqveni asaki: ")
+if name == tpl[0] or tpl[1]:
+	bibl[name] = age
+print("dictionaryshi aris: ", len(bibl), "wyvili")
